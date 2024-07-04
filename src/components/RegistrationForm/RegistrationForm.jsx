bd3f488443753bc91152a4cdeccfc50c90c44a5c
rouenemedina <rouenemedina@gmail.com>
2024-07-04 19:54:32 -0400

    created the basic folder structure for the frontend

---
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./RegistrationForm.scss";
+
+function RegistrationForm() {
+  return (
+    <>
+        <label htmlFor=""></label>
+        <input type="text" id="" className="" placeholder="" required></input>
+    </>
+  );
+}
+
+export default RegistrationForm;
---
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./SignUpPage.scss";
+
+function SignUpPage() {
+  return (
+    <main>
+      <section>
+        <h1>CREATE NEW ACCOUNT</h1>
+        <div>
+          <h3>Already a member? </h3>
+          <h3>Log In</h3>
+        </div>
+      </section>
+      <section>
+        {/* <Form /> */}
+      </section>
+    </main>
+  );
+}
+
+export default SignUpPage;

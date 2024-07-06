a96fd90f9c93cf5281081b3dbacc10dee977af32
rouenemedina <rouenemedina@gmail.com>
2024-07-06 18:40:01 -0400

    rearranged positions of header components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useState } from "react";
 import { MdClose, MdOutlineMenu } from "react-icons/md";
 import logoImg from "../../assets/logo/Logo.png";
+import userImg from "../../assets/images/daniel-lincoln-NR705beN_CU-unsplash.jpg";
 import NavBar from "../NavBar/NavBar";
 
 function Header() {
@@ -32,10 +33,12 @@ function Header() {
 
   return (
     <nav className="header">
-      <img src={logoImg} alt="PhotoNest Logo" className="header__logo"></img>
       {click ? CloseMenu : HamburgerMenu}
       {click && <NavBar isClicked={true} closeMenu={closeMenu}/>}
-      <section className="header__user"></section>
+      <img src={logoImg} alt="PhotoNest Logo" className="header__logo"></img>
+      <section className="header__user">
+        <img src={userImg} alt="User Profile Photo" className="header__img"/>
+      </section>
     </nav>
   );
 }

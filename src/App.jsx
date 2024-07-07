3f7c49676c5db7e1d45289ba8d9dd51b02e0ec1b
rouenemedina <rouenemedina@gmail.com>
2024-07-07 00:51:21 -0400

    created a route and a page for the portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SignUpPage from './pages/SignUpPage/SignUpPage';
 import LogInPage from './pages/LogInPage/LogInPage';
 import HomePage from './pages/HomePage/HomePage';
 import PhotographerPage from './pages/PhotographerPage/PhotographerPage';
+import PortfolioPage from './pages/PortfolioPage/PortfolioPage';
 
 function App() {
 
@@ -15,6 +16,8 @@ function App() {
         <Route path="/login" element={<LogInPage />}></Route>
         <Route path="/home" element={<HomePage />}></Route>
         <Route path="/profile" element={<PhotographerPage />}></Route>
+        {/* <Route path="/profile/:id" element={<PortfolioPage />}></Route> */}
+        <Route path="/portfolio" element={<PortfolioPage />}></Route>
       </Routes>
      </BrowserRouter>
     </>
---
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -0,0 +1,14 @@
+import "./PortfolioPage.scss";
+import React from "react";
+import Header from "../../components/Header/Header";
+
+function PortfolioPage() {
+  return (
+    <>
+    <Header />
+      <main></main>
+    </>
+  );
+}
+
+export default PortfolioPage;

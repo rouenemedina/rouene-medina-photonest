631095d8b172f7c663268627fb2aa6f15e22c046
rouenemedina <rouenemedina@gmail.com>
2024-07-07 22:44:17 -0400

    written what needs to be inside the dashboard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,20 @@
 import "./DashboardPage.scss";
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
 
 function DashboardPage() {
+    const [failedAuth, setFailedAuth] = useState(false);
+    const [user, setUser] = useState(null);
+
+    //login
+    //logout
+    const login = async () => {
+        //check if there is a token
+        //get token
+
+    };
+
+    //return the profile of the user (general: for photographers and clients);
     return (
         <main>
             

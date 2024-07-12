cca33a9fbc0650720ccdd8c4d8a8af2d018d6d28
rouenemedina <rouenemedina@gmail.com>
2024-07-12 08:25:21 -0400

    added conditional className for active links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,19 +9,49 @@ function NavBar({ isClicked, closeMenu }) {
   return (
     <main className={navBarClasses}>
       <section className="navBar">
-        <NavLink to="/home" className="navBar__link" onClick={closeMenu}>
+        <NavLink
+          to="/home"
+          className={({ isActive }) =>
+            isActive ? "navBar__link navBar__link--active" : "navBar__link"
+          }
+          onClick={closeMenu}
+        >
           <h4 className="navBar__title">HOME</h4>
         </NavLink>
-        <NavLink to="/home" className="navBar__link" onClick={closeMenu}>
+        <NavLink
+          to="/home"
+          className={({ isActive }) =>
+            isActive ? "navBar__link navBar__link--active" : "navBar__link"
+          }
+          onClick={closeMenu}
+        >
           <h4 className="navBar__title">PHOTOGRAPHER</h4>
         </NavLink>
-        <NavLink to="/portfolio" className="navBar__link" onClick={closeMenu}>
+        <NavLink
+          to="/portfolio"
+          className={({ isActive }) =>
+            isActive ? "navBar__link navBar__link--active" : "navBar__link"
+          }
+          onClick={closeMenu}
+        >
           <h4 className="navBar__title">PORTFOLIO</h4>
         </NavLink>
-        <NavLink to="/editportfolio" className="navBar__link" onClick={closeMenu}>
+        <NavLink
+          to="/editportfolio"
+          className={({ isActive }) =>
+            isActive ? "navBar__link navBar__link--active" : "navBar__link"
+          }
+          onClick={closeMenu}
+        >
           <h4 className="navBar__title">EDIT PORTFOLIO</h4>
         </NavLink>
-        <NavLink to="/contact" className="navBar__link" onClick={closeMenu}>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "navBar__link navBar__link--active" : "navBar__link"
+          }
+          onClick={closeMenu}
+        >
           <h4 className="navBar__title">CONTACT</h4>
         </NavLink>
       </section>

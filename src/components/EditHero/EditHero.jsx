a5a780235fda1f8f61afd5ab9b5ebd5545b5683f
rouenemedina <rouenemedina@gmail.com>
2024-07-11 20:17:56 -0400

    written pseudocode for the form submission logic

--- src/components/EditHero/EditHero.jsx
+++ src/components/EditHero/EditHero.jsx
@@ -1,27 +1,26 @@
 import PhotoUpload from "../PhotoUpload/PhotoUpload";
 import "./EditHero.scss";
 import React from "react";
+import { useState } from "react";
 
 //setup the forms
 //handle the change when there is a user input
 //handle submit or handle upload
 //handle errors
 
-function EditHero() {
-  const handleChange = () => {};
-
+function EditHero({handleChange}) {
   return (
     <main>
-      <form>
+      <form className="editHero">
         <PhotoUpload />
-        <section className="">
-          <label htmlFor="profile_description" className="">
+        <section className="editHero__container">
+          <label htmlFor="hero_description" className="editHero__label">
             DESCRIPTION:
           </label>
           <textarea
             type="textarea"
-            name="profile_description"
-            className=""
+            name="hero_description"
+            className="editHero__description"
             placeholder="Tell us about yourself."
             // value={}
             onChange={handleChange}
--- src/pages/EditPortfolioPage/EditPortfolioPage.jsx
+++ src/pages/EditPortfolioPage/EditPortfolioPage.jsx
@@ -4,7 +4,25 @@ import Header from "../../components/Header/Header";
 import EditHero from "../../components/EditHero/EditHero";
 import Buttons from "../../components/Buttons/Buttons";
 
+const API_URL = import.meta.env.VITE_APP_API_URL;
+
 function EditPortfolioPage() {
+  const [editHeroFormData, setEditHeroFormData] = useState({
+    hero_description: "",
+  });
+  //this is for handling errors in the form
+  const [formErrors, setFormErrors] = useState({});
+  //this is for errors in the axios call
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setEditHeroFormData({ ...editHeroFormData, [name]: value })
+  };
+
+  //to store the updated formData to a variable
+  //post endpoint
+  //handle reset
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
@@ -14,7 +32,11 @@ function EditPortfolioPage() {
       <Header />
       <main>
         <form onSubmit={handleSubmit}>
-          <EditHero />
+          <EditHero
+            editHeroFormData={editHeroFormData}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+          />
         </form>
         <Buttons showSubmit />
       </main>

9564de9d79af902d765107f084d89f0633b93134
rouenemedina <rouenemedina@gmail.com>
2024-07-14 22:38:07 -0400

    added an import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import LogInPage from './pages/LogInPage/LogInPage';

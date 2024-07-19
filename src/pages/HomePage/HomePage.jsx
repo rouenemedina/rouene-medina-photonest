b614deb6afd0e884a9616695afbc3ba8cc370dba
rouenemedina <rouenemedina@gmail.com>
2024-07-19 11:46:38 -0400

    added a footer component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,16 +4,20 @@ import logoImg from "../../assets/logo/Logo-2.png";
 import Header from "../../components/Header/Header";
 import HomePageHero from "../../components/HomePageHero/HomePageHero";
 import HomePageFeature from "../../components/HomePageFeature/HomePageFeature";
+import Footer from "../../components/Footer/Footer";
 
 function HomePage() {
   return (
     <>
       <header className="homepage__header">
         <Header />
-        <img src={logoImg} className="homepage__logo"></img>
+        {/* <img src={logoImg} className="homepage__logo"></img> */}
         <HomePageHero />
-        <HomePageFeature />
       </header>
+      <main>
+        <HomePageFeature />
+      </main>
+      <Footer />
     </>
   );
 }
